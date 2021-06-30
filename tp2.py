9a129f1d22724191b60404c33258f2ff89378743
root <root@localhost.localdomain>
2021-06-30 00:23:07 -0700

    commit tp2

--- tp2.py
+++ tp2.py
@@ -1,4 +1,4 @@
-print("donnez votre age")
-age = int(intput("age:"))
-jours = age * 365
-print("votre age en jours est", jours, "jours")
+print("entrez votre age svp")
+age = int(input("age :"))
+jours = age * 365 
+print("votre age est de", jours, "jours")
