153487ef8703b99cc48b75fba625cd6961cf1730
root <root@localhost.localdomain>
2021-06-30 02:37:13 -0700

    commit tp2 avec modif

--- tp2.py
+++ tp2.py
@@ -1,4 +1,4 @@
-print("entrez votre age svp")
-age = int(input("age :"))
-jours = age * 365 
-print("votre age est de", jours, "jours")
+import datetime
+
+date = datetime.datetime.now()
+print(date)
